62cd8bacfbd8cb1c668f2298da7e2f03d5f80331
mansha71 <manansharm1010@gmail.com>
2024-04-06 15:37:26 -0400

    adding mainPage

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,20 +11,7 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-// @react-native-async-storage/async-storage
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-// @react-native-community/masked-view
-import org.reactnative.maskedview.RNCMaskedViewPackage;
-// @react-native-firebase/app
-import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
-// react-native-gesture-handler
-import com.swmansion.gesturehandler.RNGestureHandlerPackage;
-// react-native-reanimated
-import com.swmansion.reanimated.ReanimatedPackage;
-// react-native-safe-area-context
-import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
-// react-native-screens
-import com.swmansion.rnscreens.RNScreensPackage;
+
 
 public class PackageList {
   private Application application;
@@ -69,14 +56,7 @@ public class PackageList {
 
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
-      new MainReactPackage(mConfig),
-      new AsyncStoragePackage(),
-      new RNCMaskedViewPackage(),
-      new ReactNativeFirebaseAppPackage(),
-      new RNGestureHandlerPackage(),
-      new ReanimatedPackage(),
-      new SafeAreaContextPackage(),
-      new RNScreensPackage()
+      new MainReactPackage(mConfig)
     ));
   }
 }
