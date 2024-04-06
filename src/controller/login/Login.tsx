a6eb64319cc735bb30ad4e6a97595cb57922f582
Khalid Farag <faragk1@mcmaster.ca>
2024-04-06 16:39:26 -0400

    Set up the userInformation model

--- src/controller/login/Login.tsx
+++ src/controller/login/Login.tsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from '../firebase.js';
 import { setDoc, doc } from 'firebase/firestore';
+import UserInformation from '../../model/UserInformation.js';
 
 export const Login = async (email: string, password: string) => {
     try {
@@ -14,13 +15,20 @@ export const Login = async (email: string, password: string) => {
     }
 }
 
-export const Register = async (email: string, password: string) => {
+export const Register = async (userInfo: UserInformation) => {
     try {
-        createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        createUserWithEmailAndPassword(auth, userInfo.email, userInfo.password).then((userCredential) => {
             const user = userCredential.user;
+            userInfo.employeeID = user.uid;
             setDoc(doc(db, 'users', user.uid), {
-                email: user.email,
-                uid: user.uid
+                email: userInfo.email,
+                employeeID: userInfo.employeeID,
+                name: userInfo.name,
+                section: userInfo.section,
+                title: userInfo.title,
+                phone: userInfo.phoneNum,
+                dateOfBirth: userInfo.DOB,
+                team: userInfo.teamName
             });
             console.log(user);
             return user;
