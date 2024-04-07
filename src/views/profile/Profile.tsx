89e2c5e7c07fa4e5fb53cd68b7f9817cef1d036c
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-04-07 02:19:45 -0400

    fixed import

--- src/views/profile/Profile.tsx
+++ src/views/profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { SafeAreaView, Text, StyleSheet, View, TouchableOpacity, TextInput, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { getAccountDetails, changeComapanySection, changeJobTitle, changePhone, changeTeamName } from '../../controller/AccountManager/AccountManager';
+import { getAccountDetails, changeComapanySection, changeJobTitle, changePhone, changeTeamName } from '../../controller/accountManager/AccountManager.tsx';
 
 const ProfilePage: React.FC = () => {
   const navigation = useNavigation();
@@ -126,55 +126,33 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginTop: 20,
   },
-  searchInput: {
-    width: '90%',
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-  },
-  employeeList: {
+  scrollContainer: {
     flex: 1,
     width: '100%',
   },
-  employeeItem: {
+  detailsContainer: {
     padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
   },
-  employeeName: {
-    fontSize: 20,
+  label: {
+    fontSize: 18,
     fontWeight: 'bold',
-  },
-  employeeDepartment: {
-    fontSize: 16,
-    color: 'grey',
-  },
-  navBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-    backgroundColor: '#f0f0f0',
-    padding: 10,
-  },
-  navButton: {
-    padding: 10,
-  },
-  messageButton: {
-    backgroundColor: '#007bff',
-    borderRadius: 5,
-    paddingVertical: 8,
-    paddingHorizontal: 20,
     marginTop: 10,
-    alignSelf: 'flex-start',
   },
-  messageButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+  fieldContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    padding: 10,
+    marginVertical: 10,
+  },
+  updateButton: {
+    color: 'blue',
+    fontSize: 16,
   },
 });
 
