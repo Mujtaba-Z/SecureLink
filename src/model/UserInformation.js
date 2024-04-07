679e51869a4ef27484346620896481e96d96d8f7
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2024-04-07 12:09:07 -0400

    small user change

--- src/model/UserInformation.js
+++ src/model/UserInformation.js
@@ -1,5 +1,5 @@
 class UserInformation {
-    constructor(employeeID, name, email, password, section, title, phoneNum, teamName, DOB ) {
+    constructor(employeeID, name, email, password, section, title, phoneNum, teamName, DOB, leaderboardPoints) {
         this.name = name;
         this.email = email;
         this.password = password;
@@ -9,6 +9,7 @@ class UserInformation {
         this.teamName = teamName;
         this.DOB = DOB;
         this.employeeID = employeeID;
+        this.leaderboardPoints = leaderboardPoints;
     }
 }
 
