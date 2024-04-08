fc3a4d2bfed4dc612439214e859802dfbf16bf1e
Khalid Farag <faragk1@mcmaster.ca>
2024-04-08 01:42:32 -0400

    Created the awards and communication models

---
+++ src/model/CommunicationSession.js
@@ -0,0 +1,10 @@
+class CommunicationSession{
+    constructor(chatter1, chatter2, chatLog, key){
+        this.chatter1 = chatter1;
+        this.chatter2 = chatter2;
+        this.chatLog = chatLog;
+        this.key = key;
+    }
+}
+
+module.exports = CommunicationSession;
